1018a6092d35404461cd48bace2aab877f070e1d
Yuri-1 <72656487+Yuri-1@users.noreply.github.com>
2021-02-03 13:19:36 +0800

    Create yanghui.java
    杨辉三角形

---
+++ yanghui.java
@@ -0,0 +1,35 @@
+public class yanghui
+{
+    public static int[][] creat(final int n)
+    {
+        int [][] mat = new int[n][];
+        
+            for (int i=0;i<n;i++)
+            {
+                mat[i] = new int[i+1];
+                mat[i][0] = mat[i][i] = 1;
+                for (int j=1;j<i;j++)
+                {
+                    mat[i][j] = mat[i-1][j-1] + mat[i-1][j];
+                }
+            }
+            return mat;
+        
+    }
+
+    public static void print(int[][] mat)
+    {
+        for (int i=0;i<mat.length;i++)
+        {
+            System.out.print(String.format("%"+(mat.length-i+1)*2+"c",' '));
+            for (int j=0;j<mat[i].length;j++)
+                System.out.print(String.format("%4d",mat[i][j]));
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        yanghui.print(yanghui.creat(4));
+    }
+
+
+}
